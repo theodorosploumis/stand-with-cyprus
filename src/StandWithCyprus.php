bdedc44140ad4f97bdba1d9159fe732ea4583b78
Theodoros Ploumis <me@theodorosploumis.com>
2023-06-10 15:17:25 +0300

    Convert to single Command Application, fix vendor path

--- src/StandWithCyprus.php
+++ src/StandWithCyprus.php
@@ -2,12 +2,12 @@
 
 <?php
 
+require __DIR__.'/../vendor/autoload.php';
+
 use Symfony\Component\Console\Exception\ExceptionInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\SingleCommandApplication;
-
-
 
 try {
     (new SingleCommandApplication())
